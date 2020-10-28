46a82d72e5e3e195d997db5a351f986ef05ba047
Francesco <francescofacca@gmail.com>
2020-10-28 16:56:02 +0000

    rspec test to check instance for docking_station and check if bike works

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -5,14 +5,15 @@ describe DockingStation do
 
   it { docking_station.should respond_to(:release_bike) }
 
-  # bike = docking_station.release_bike
-  it { docking_station.release_bike.should respond_to(:working?)}
-
-  it "release_bike produces a new instance of the Bike class" do
-       expect(docking_station.release_bike).to be_instance_of(Bike)
-  end
 
   it "release_bike produces a new instance of the Bike class" do
     expect(docking_station.release_bike).to be_instance_of(Bike)
+  end
+
+  # bike = docking_station.release_bike
+  it { docking_station.release_bike.should respond_to(:working?)}
+
+  it "checks the bike is working" do
+      expect(docking_station.release_bike.working?).to eq true
   end
 end
